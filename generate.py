111db3929c129e86fbe4a5e7ea3a58de791c8496
mattsteinberg13 <64422669+mattsteinberg13@users.noreply.github.com>
2024-09-19 17:31:11 +0200

    Add files via upload

---
+++ generate.py
@@ -0,0 +1,14 @@
+import numpy as np
+from OperatorPush.NetworkToolbox import create_layer_q4, assign_layers_to_tensors
+from OperatorPush.TensorToolbox import ensure_minimum_legs, add_logical_legs, get_tensor_from_id, Tensor, has_logical
+
+class Generate:
+
+    def __init__(self, ):
+
+        """
+        Args:
+
+        """
+
+        ...
