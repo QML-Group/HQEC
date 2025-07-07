b1d5b0784ac5fec660230cf578a93b09622b2aa2
Junyu <fzhjerry@126.com>
2025-07-08 01:43:22 +0800

    Temporarily removed the unfinished external wrapper generate.py

--- decoding.py
+++ decoding.py
@@ -16,7 +16,8 @@ class Decode:
             time_limit: float
             pauli_mixing_vector: tuple where (rx, ry, rz) are relative Pauli proportions
         """
-        self.error_rate_range = self.gen_error_rates(error_rate_start=error_rate_start, error_rate_end=error_rate_end, number_spaces=number_spaces)
+        self.error_rate_range = self.gen_error_rates(error_rate_start=error_rate_start, error_rate_end=error_rate_end,
+                                                     number_spaces=number_spaces)
         self.stabilizers = stabilizers
         self.x_logicals = x_logicals
         self.z_logicals = z_logicals
@@ -29,15 +30,39 @@ class Decode:
 
     def erasure(self, mc_trials: int):
         logicals = self.z_logicals + self.x_logicals
-        recovery_rates = ErasureDecoder.calculate_recovery_rates_for_p_range(n=mc_trials, p_start=self.error_rate_range[0], p_end=self.error_rate_range[-1], p_step=self.error_rate_range[1] - self.error_rate_range[0], stabilizers=self.stabilizers,logical_operators=logicals, n_process=1, cpu_affinity_list=None)
+        recovery_rates = ErasureDecoder.calculate_recovery_rates_for_p_range(n=mc_trials,
+                                                                             p_start=self.error_rate_range[0],
+                                                                             p_end=self.error_rate_range[-1],
+                                                                             p_step=self.error_rate_range[1] -
+                                                                                    self.error_rate_range[0],
+                                                                             stabilizers=self.stabilizers,
+                                                                             logical_operators=logicals,
+                                                                             n_process=1, cpu_affinity_list=None)
         return recovery_rates
 
     def integer_optimization(self, mc_trials: int):
-        recovery_rates = PauliDecoder.quantum_error_correction_decoder(tensor_list=self.tensor_list, stabilizers=self.stabilizers, logical_xs=self.x_logicals, logical_zs=self.z_logicals, px=self.pauli_mixing_vector[0], py=self.pauli_mixing_vector[1], pz=self.pauli_mixing_vector[2], N=mc_trials, time_limit=None, mip_focus=0, heuristics=0, output_flag=0)
+        recovery_rates = PauliDecoder.quantum_error_correction_decoder(tensor_list=self.tensor_list,
+                                                                       stabilizers=self.stabilizers,
+                                                                       logical_xs=self.x_logicals,
+                                                                       logical_zs=self.z_logicals,
+                                                                       px=self.pauli_mixing_vector[0],
+                                                                       py=self.pauli_mixing_vector[1],
+                                                                       pz=self.pauli_mixing_vector[2],
+                                                                       N=mc_trials, time_limit=None, mip_focus=0,
+                                                                       heuristics=0, output_flag=0)
 
     def tensor_network(self, mc_trials: int):
         recovery_rates = []
         for phys_error_rate in self.error_rate_range:
-            success_rate = TN_decoder.tn_quantum_error_correction_decoder_multiprocess(tensor_list=self.tensor_list, p=phys_error_rate, rx=self.pauli_mixing_vector[0], ry=self.pauli_mixing_vector[1], rz=self.pauli_mixing_vector[2], N=mc_trials, stabilizers=self.stabilizers, logical_x=None, logical_z=None, n_process=1, cpu_affinity_list=None, f=None)
+            success_rate = TN_decoder.tn_quantum_error_correction_decoder_multiprocess(tensor_list=self.tensor_list,
+                                                                                       p=phys_error_rate,
+                                                                                       rx=self.pauli_mixing_vector[0],
+                                                                                       ry=self.pauli_mixing_vector[1],
+                                                                                       rz=self.pauli_mixing_vector[2],
+                                                                                       N=mc_trials,
+                                                                                       stabilizers=self.stabilizers,
+                                                                                       logical_x=None, logical_z=None,
+                                                                                       n_process=1,
+                                                                                       cpu_affinity_list=None, f=None)
 
             recovery_rates.append(success_rate)
